9782c3c1ca7dfd64a8d5b3df728fa8307a467d73
Aleksy Rembicki <Aleksy.Rembicki.c@action1.com>
2026-07-15 13:38:45 +0200

    Fix headers based on PSScriptAnalyzer rule

--- Private/ConvertTo-LatinAlphaNumericString.ps1
+++ Private/ConvertTo-LatinAlphaNumericString.ps1
@@ -1,10 +1,9 @@
 # Action1 Public Repository Material
-# Subject to TERMS_OF_USE.md
-# https://github.com/Action1Corp/PSAction1/blob/main/TERMS_OF_USE.md
+# Subject to TERMS_OF_USE.md (https://github.com/Action1Corp/PSAction1/blob/main/TERMS_OF_USE.md)
 # Provided AS IS
 # Use at your own risk
 # Review and test before production deployment
-# (c) Action1 Corporation
+# © Action1 Corporation
 
 function ConvertTo-LatinAlphaNumericString {
     [CmdletBinding()]
