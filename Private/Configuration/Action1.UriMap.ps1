dd7a6ebdf8478262ae5f4f8449b09383b786fb6e
Aleksy Rembicki <Aleksy.Rembicki.c@action1.com>
2026-08-13 03:06:12 +0200

    Update section names in URI map

--- Private/Configuration/Action1.UriMap.ps1
+++ Private/Configuration/Action1.UriMap.ps1
@@ -15,21 +15,21 @@ $Script:Action1_UriMap = @{
     G_Enterprise           = { "/enterprise" }
     U_Enterprise           = { "/enterprise" }
 
-    # Organizations
+    # Organization
     G_Organization         = { param($Org_ID) "/organizations/$Org_ID" }
     G_Organizations        = { "/organizations" }
     N_Organization         = { "/organizations" }
     U_Organization         = { param($Org_ID) "/organizations/$Org_ID" }
     D_Organization         = { param($Org_ID) "/organizations/$Org_ID" }
 
-    # Endpoints
+    # Endpoint
     G_AgentDeployment      = { param($Org_ID) "/endpoints/discovery/$Org_ID" }
     G_Endpoints            = { param($Org_ID) "/endpoints/managed/$Org_ID" }
     G_Endpoint             = { param($Org_ID, $Object_ID) "/endpoints/managed/$Org_ID/$Object_ID" }
     U_Endpoint             = { param($Org_ID, $Object_ID) "/endpoints/managed/$Org_ID/$Object_ID" }
     D_Endpoint             = { param($Org_ID, $Object_ID) "/endpoints/managed/$Org_ID/$Object_ID" }
 
-    # Endpoint groups
+    # EndpointGroup
     G_EndpointGroupMembers = { param($Org_ID, $Object_ID)"/endpoints/groups/$Org_ID/$Object_ID/contents" }
     G_EndpointGroup        = { param($Org_ID, $Object_ID) "/endpoints/groups/$Org_ID/$Object_ID" }
     G_EndpointGroups       = { param($Org_ID) "/endpoints/groups/$Org_ID" }
@@ -37,41 +37,41 @@ $Script:Action1_UriMap = @{
     U_GroupModify          = { param($Org_ID, $Object_ID) "/endpoints/groups/$Org_ID/$Object_ID" }
     U_GroupMembers         = { param($Org_ID, $Object_ID) "/endpoints/groups/$Org_ID/$Object_ID/contents" }
 
-    # Apps
+    # App
     G_Apps                 = { param($Org_ID) "/apps/$Org_ID/data" }
     G_EndpointApps         = { param($Org_ID, $Object_ID) "/apps/$Org_ID/data/$Object_ID" }
     R_InstalledSoftware    = { param($Org_ID, $Object_ID) "/apps/$Org_ID/requery/$Object_ID" }
 
-    # Packages
+    # Package
     G_Packages             = { "/packages/all" }
     G_PackageVersions      = { param($Object_ID) "/software-repository/all/$Object_ID`?fields=versions" }
     G_Scripts              = { "/scripts/all" }
     N_DeploySoftware       = { param($Org_ID)  "/policies/instances/$Org_ID" }
 
-    # Automations
+    # Automation
     G_AutomationInstances  = { param($Org_ID, $Object_ID) "/automations/instances/$Org_ID`?endpoint_id=$Object_ID" }
     G_Automations          = { param($Org_ID) "/policies/schedules/$Org_ID" }
     N_Automation           = { param($Org_ID)  "/policies/schedules/$Org_ID" }
     N_DeferredRemediation  = { param($Org_ID)  "/policies/schedules/$Org_ID" }
     U_Automation           = { param($Org_ID, $Object_ID)  "/policies/schedules/$Org_ID/$Object_ID" }
 
-    # Policies
+    # Policy
     G_Policy               = { param($Org_ID, $Object_ID) "/policies/instances/$Org_ID/$Object_ID" }
     G_Policies             = { param($Org_ID)  "/policies/instances/$Org_ID" }
     G_PolicyResults        = { param($Org_ID, $Object_ID) "/policies/instances/$Org_ID/$Object_ID/endpoint_results" }
     N_Remediation          = { param($Org_ID)  "/policies/instances/$Org_ID" }
 
-    # Reports
+    # Report
     G_ReportData           = { param($Org_ID, $Object_ID)"/reportdata/$Org_ID/$Object_ID/data" }
     G_ReportExport         = { param($Org_ID, $Object_ID)"/reportdata/$Org_ID/$Object_ID/export" }
     G_Reports              = { "/reports/all" } 
     R_ReportData           = { param($Org_ID, $Object_ID) "/reportdata/$Org_ID/$Object_ID/requery" }
 
-    # Updates
+    # Update
     G_MissingUpdates       = { param($Org_ID) "/updates/$Org_ID" }
     R_InstalledUpdates     = { param($Org_ID) "/updates/installed/$Org_ID/requery" }
 
-    # Vulnerabilities
+    # Vulnerability
     G_Vulnerabilities              = { param($Org_ID) "/vulnerabilities/$Org_ID" }
     G_Vulnerability                = { param($Org_ID, $CVEId) "/vulnerabilities/$Org_ID/$CVEId" }
     G_VulnerabilityEndpoints       = { param($Org_ID, $CVEId) "/vulnerabilities/$Org_ID/$CVEId/endpoints" }
